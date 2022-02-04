aa95c6133db20d351d7c849855362e91ad0699b7
Dawid930 <davidnyikes@gmail.com>
2022-02-04 18:02:35 +0100

    friday-task

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+function returnParam(parameter) {
+    //console.log(parameter)
+    return `<h1>${parameter}</h1>`
+}
+
+//returnParam("argument")
+
+console.log( returnParam("hello") )
+
+
+
+
+//ez a megoldasa a 3. feladatnak, de a toString(), ahogy en csinaltam sz is jo megodlas   parameter.toString()
